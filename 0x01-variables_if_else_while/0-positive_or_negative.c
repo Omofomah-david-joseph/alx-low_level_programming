587cb386cffde0e538efa9c4e04d221e920f74ba
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-08 01:44:19 -0700

    if..else statement identifying positive and negative numbers from any given random numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+/*
+ * FILE: 0-Positive_or_negative.C
+ * Auth: Omofomah-david-joseph
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+/**
+ * main - Print a random number and indicate if its positive or negative
+ *
+ * Return: Always 0 (success)
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        if( n > 0 )
+               printf("%d is positive\n",n);
+        else if( n == 0 )
+                printf("%d is zero\n",n);
+        else ( n < 0 );
+                printf("%d is negative\n",n);
+        return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Project on C-Variables, If...else, while statements
