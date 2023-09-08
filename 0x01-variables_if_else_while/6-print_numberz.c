13186a6792a165cab7222cbfd90ff4d8bc017438
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-08 12:27:53 -0700

    Printing all single digit of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+/*
+ * File: 6-print_numberz.c
+ * Auth: Omofomah-david-joseph
+ */
+
+#include <stdio.h>
+
+/**
+ * main -Printing all single digits of base 10
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <=9; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
