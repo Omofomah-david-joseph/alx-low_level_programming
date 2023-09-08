2223db656ce06bbec535042419a643ea88fccdc4
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-08 04:12:56 -0700

    A program that prints alphabet in lowercases leaving out some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Omofomah-david-joseph
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Program that prints the alphabet in lowercases leaving out q and e
+ *
+ * Return: Always0 (success)
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
