8d6c9dfae06195be3a300fbd7a87d15b8709c188
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-08 11:21:02 -0700

    Program to print alphabets in lowercases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Omofomah-david-joseph
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Print alphabets in lower case
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        char letter;
+
+        for (letter = 'a'; letter <= 'z'; letter++)
+                putchar(letter);
+
+        putchar('\n');
+
+        return (0);
+}
