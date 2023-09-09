a3d21a20ed285208d7fd0e01ad4a3f0096a58b60
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-09 00:16:26 -0700

    Printing all possible combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+/*
+ * File: 100-print_comb3.c
+ * Auth: Omofomah-david-joseph
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Printing all possible different combination of two digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, o;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (o = 0; o < 10; o++)
+		{
+			putchar(i + '0');
+			putchar(o + '0');
+			if (i == 8 && o == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
