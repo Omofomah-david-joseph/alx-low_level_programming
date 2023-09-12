95f7874b916bc6d74fa671c5b30286962097b830
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-12 04:03:44 -0700

    Printing -putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Printing -putchar followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	char *sh - "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	putchar('\n');
+	return (0);
+}
