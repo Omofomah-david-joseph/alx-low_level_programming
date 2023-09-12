3e7b663fd9986d4422a630090f92acbbf14f786c
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-12 07:28:57 -0700

    _putchar task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('P');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar ('\n');
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
 	return(0);
 }
