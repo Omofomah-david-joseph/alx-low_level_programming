bf428c20a7137c7227c9f490f2e8b851b72156c4
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-17 12:56:48 -0700

    print more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - checks for numbers between 0 -9
+ *
+ * Return: Always 0 (success)
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
+	}
+	_putchar('\n');
+}
