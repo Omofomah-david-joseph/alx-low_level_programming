7873949b83bbe56288e976ccf559e92d78b75016
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-17 08:55:30 -0700

    multiplying tow integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: first interger value
+ * @b: second value
+ * Return: Always 0 (success)
+ */
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+
+	return (mult);
+}
