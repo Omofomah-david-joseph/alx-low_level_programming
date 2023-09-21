d51bd28e5422bab7bedbf6956d9f3f644ed54072
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-21 06:41:14 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -18,7 +18,7 @@ char *string_toupper(char *);
 char *leet(char *);
 char *cap_string(char *s);
 char *_strncat(char *dest, char *src, int n);
-char *rot13(char *);
+char *rot13(char *s);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
