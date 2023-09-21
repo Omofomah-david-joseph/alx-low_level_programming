ddbc3c59c3250f32cbe5f9a60a23245711ac90e2
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-21 06:11:04 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,30 +9,28 @@
  */
 char *cap_string(char *s)
 {
-	int i, j;
-	int a[] = {32, 9, 10, 44, 59, 46, 33, 66, 43, 40, 41, 123, 125};
+	int index = 0;
 
-	i = 0;
-	while (*(s + i) != '\0')
+	while (s[index])
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-		{
-			if (i == 0)
-			{
-				*(s + i) = *(s + i) - 32;
-			}
-			else
-			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - 1))
-					{
-						*(s + i) = *(s + i) - 32;
-					}
-				}
-			}
-		}
-		i++;
+		while (!(s[index] >= 'a' && s[index] <= 'z'))
+			index++;
+		if (s[index - 1] == ' ' ||
+		s[index - 1] == '\t' ||
+		s[index - 1] == '\n' ||
+		s[index - 1] == ',' ||
+		s[index - 1] == ';' ||
+		s[index - 1] == '.' ||
+		s[index - 1] == '!' ||
+		s[index - 1] == '?' ||
+		s[index - 1] == '"' ||
+		s[index - 1] == '(' ||
+		s[index - 1] == ')' ||
+		s[index - 1] == '{' ||
+		s[index - 1] == '}' ||
+		index == 0)
+			s[index] -= 32;
+		index++;
 	}
 	return (s);
 }
