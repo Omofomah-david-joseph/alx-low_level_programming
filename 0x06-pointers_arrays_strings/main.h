be547950813819f51be3e0f8ac3a94d4b7bdb89a
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-21 00:56:28 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -18,5 +18,7 @@ char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
 char *leet(char *);
+char *cap_string(char *);
+
 
 #endif /* MAIN_H*/
