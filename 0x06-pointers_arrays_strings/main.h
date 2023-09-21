22dac2d89813a98373ca08cb611cd43b58f61e08
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-09-21 06:29:51 -0700

    rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,14 +10,19 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n)
 int _atoi(char *s);
+
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
 char *leet(char *);
 char *cap_string(char *s);
 char *_strncat(char *dest, char *src, int n);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+char *_strcpy(char *dest, char *src);
 
 #endif /* MAIN_H*/
