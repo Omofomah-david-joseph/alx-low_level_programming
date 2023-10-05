c3edc2ba201c7f21dce945d4b5c878b400490462
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-10-05 07:27:40 -0700

    main.h

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int check_num(char *str);
+int _atoi(char *s);
+
+
+#endif /* MAIN_H*/
