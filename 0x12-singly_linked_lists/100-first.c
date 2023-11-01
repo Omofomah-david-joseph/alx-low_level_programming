5427fb9390f31d8683c6152cf344f753625876fd
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-11-01 03:00:54 -0700

    a programme function that prints You're beat! and yet, you must allow,I bore my house upon my back,before the main function is executed.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void comefirst(void)__attribute__((constructor));
  */
 void comefirst(void)
 {
-	printf("You're beat! and yet, you must allow \n");
+	printf("You're beat! and yet, you must allow, \n");
 	printf("I bore my house upon my back!\n");
 }
