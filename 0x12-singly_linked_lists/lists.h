86c3ad8e8a04ef93f316ad7e264b7e75b73607d2
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-11-01 03:08:42 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,7 +25,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void comefirst(void);
+void ComeFirst(void);
 list_t *create_node(const char *str);
 int len(const char *str);
 
